b4137a368275054ae0870b1734ab80830f9a7bcf
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-16 17:40:59 -0500

    Delete 2.- MENU_ENCARGADOS.vb

--- 2.- MENU_ENCARGADOS.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class menuEncargado
-    Private Sub MENU_ENCARGADOS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub botonPuntoDeVenta_E_Click(sender As Object, e As EventArgs) Handles botonPuntoDeVenta_E.Click
-
-    End Sub
-End Class
