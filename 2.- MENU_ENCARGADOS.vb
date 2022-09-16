b0341ea96e832f452cad4d7c3845b045116f371e
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-16 17:15:34 -0500

    Add files via upload

---
+++ 2.- MENU_ENCARGADOS.vb
@@ -0,0 +1,9 @@
+﻿Public Class menuEncargado
+    Private Sub MENU_ENCARGADOS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub botonPuntoDeVenta_E_Click(sender As Object, e As EventArgs) Handles botonPuntoDeVenta_E.Click
+
+    End Sub
+End Class
---
+++ 4.- GENERAR_REPORTE.vb
@@ -0,0 +1,5 @@
+﻿Public Class generarReporte
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
