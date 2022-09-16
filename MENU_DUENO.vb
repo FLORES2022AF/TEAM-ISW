d8758f9829d71b0639a1ddc1c6f2112fa4d2810e
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-16 05:50:29 -0500

    Add files via upload

---
+++ MENU_DUENO.vb
@@ -0,0 +1,9 @@
+﻿Public Class menuDueno
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub botonGenerarPrelistaDeProductos_Click(sender As Object, e As EventArgs) Handles botonGenerarPrelistaDeProductos.Click
+
+    End Sub
+End Class
