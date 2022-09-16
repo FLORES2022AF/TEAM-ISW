2564779c3d03c9fea44a9970b79c6c16361154eb
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-16 17:41:58 -0500

    Delete 5.- GESTIONAR_USUARIO.vb

--- 5.- GESTIONAR_USUARIO.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class GESTIONAR_USUARIO
-    Private Sub etiquetaApellidoPaterno_Click(sender As Object, e As EventArgs) Handles etiquetaApellidoPaternoUsuario.Click
-
-    End Sub
-End Class
