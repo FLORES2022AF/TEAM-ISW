4db066ee2c6e565774b75c9953655f1c92528cbb
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-16 17:14:27 -0500

    Delete MENU_DUENO.vb

--- MENU_DUENO.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class menuDueno
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub botonGenerarPrelistaDeProductos_Click(sender As Object, e As EventArgs) Handles botonGenerarPrelistaDeProductos.Click
-
-    End Sub
-End Class
