8c1d13f4373f57c62064d4c5e45eaebb588c8bfb
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-27 14:28:50 -0500

    Delete 6.- GESTIONAR_INVENTARIO.vb

--- 6.- GESTIONAR_INVENTARIO.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class gestionarInventario
-    Private Sub gestionarInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub botonDarDeAltaArticulo_Click(sender As Object, e As EventArgs) Handles botonDarDeAltaArticulo.Click
-
-    End Sub
-End Class
