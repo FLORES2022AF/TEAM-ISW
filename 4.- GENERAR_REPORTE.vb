1be90d9b9e0ad1a06f06b4f8cc2d60cdf025a726
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-27 14:27:16 -0500

    Delete 4.- GENERAR_REPORTE.vb

--- 4.- GENERAR_REPORTE.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class generarReporte
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles tablReportes.CellContentClick
-
-    End Sub
-End Class
