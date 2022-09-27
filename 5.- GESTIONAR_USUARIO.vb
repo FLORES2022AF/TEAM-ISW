5b49d852640261ba5309637db15e4edbefb4f611
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-27 14:28:15 -0500

    Delete 5.- GESTIONAR_USUARIO.vb

--- 5.- GESTIONAR_USUARIO.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class gestionarUsuario
-    Private Sub etiquetaApellidoPaterno_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
