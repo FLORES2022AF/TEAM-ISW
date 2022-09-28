41715953fe19fa2c5ad28270870fd6e17f81ed0c
FLORES2022AF <113616171+FLORES2022AF@users.noreply.github.com>
2022-09-28 14:15:25 -0500

    Add files via upload

---
+++ PRODUCTO II/DISEÑO FINAL v2.0/5.- GESTIONAR_USUARIO.vb
@@ -0,0 +1,9 @@
+﻿Public Class gestionarUsuario
+    Private Sub etiquetaApellidoPaterno_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
---
+++ PRODUCTO II/DISEÑO FINAL v2.0/6.- GESTIONAR_INVENTARIO.vb
@@ -0,0 +1,9 @@
+﻿Public Class gestionarInventario
+    Private Sub gestionarInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub botonDarDeAltaArticulo_Click(sender As Object, e As EventArgs) Handles botonDarDeAltaArticulo.Click
+
+    End Sub
+End Class
